8fe9cccd93877f081e875dbb6686e7e54be896c1
Trevor B <trevor@mtaonline.net>
2016-07-18 02:45:01 -0800

    Modify class so Tables initialize with empty players and Players initialize with a type of 'empty'

--- Player.rb
+++ Player.rb
@@ -1,15 +1,29 @@
+require './settings.rb'
+
 class Player
 
-	def initialize
-
+	def initialize()
+		@name = 'Empty'
 		@chips = 0
 		@all_in = false
-		@type = 'computer'
+		@type = 'empty'
 		@hand = []
 	end
 
-	def make_player_live()
+	def to_s
+		@name
+	end
+
+	def make_live_player(chips)
 		@type = 'live'
+		@chips += chips
+		@name = $names[rand(0...$names.length)]
+	end
+
+	def make_computer_player(chips)
+		@type = 'computer'
+		@chips += chips
+		@name = $names[rand(0...$names.length)]
 	end
 
 	def add_card(card)
@@ -17,17 +31,30 @@ class Player
 	end
 
 	def show_hand()
-		@hand.each{ |card| print card.display() + " " }
-		print "\n"
+		@hand.each do |card|
+			print card.display() + " "
 		end
+		print "\n"
 	end
 
 	def bet
+		puts "A bet was made."
 	end
 
 	def call
+		puts "A call was made."
 	end
 
 	def raise
+		puts "A bet was made."
+	end
+
+	def fold
+		puts "You hand was folded"
+		@hand = []
+	end
+
+	def win(pot)
+		@chips += pot
 	end
 end
--- Table.rb
+++ Table.rb
@@ -4,9 +4,33 @@ require './Game.rb'
 class Table
 
 	def initialize(seats)
-		@players = Array.new(seats)
+		i = 0
+		@players = []
+		while i < seats
+			@players.push(Player.new())
+			i += 1
+		end
+
 		@dealer_button = rand(0...seats)
 		@game = "Texas Hold'em"
+	end
+
+	def display_players()
+		puts "Current Players"
+		@players.each do |val|
+			puts val
+		end
+	end
+
+	def get_open_seat()
+		i = 0
+		while i < @players.length
+			if @players[i] == 'Empty'
+				return i
+			end
+			i += 1
+		end
+		return -1
 	end
 
 	def run()
@@ -15,16 +39,30 @@ class Table
 		puts "2. Add a live player"
 		puts "3. Play a hand of " + @game
 		puts "4. Quit"
+
+		self.display_players()
+
 		choice = $stdin.gets.chomp
 
 		if choice == "1"
-			self.add_player(Player.new)
+
+			seat = self.get_open_seat()
+
+			player = Player.new
+			player.make_computer_player(100)
+
+			self.add_player(Player.new, seat)
 			self.run()
 
 		elsif choice == "2"
+
+			puts "Where would you like to sit?"
+			seat = $stdin.gets.chomp
+
 			player = Player.new
-			player.make_player_live()
-			self.add_player(player)
+			player.make_live_player(100)
+
+			self.add_player(player, seat.to_i)
 			self.run()
 
 		elsif choice == "3"
@@ -43,14 +81,13 @@ class Table
 			puts "Invalid choice please select again"
 			self.run()
 		end
-
 	end
 
-	def add_player(player)
-		@players.push(player)
+	def add_player(player, seat)
+		@players[seat] = player
 	end
 
 	def advance_button
-		@dealer_button = (@dealer_button + 1) % 9
+		@dealer_button = (@dealer_button + 1) % @players.length
 	end
 end
