4889bbf6757e8202286d1959e797fb33cd80a516
Trevor B <trevor@mtaonline.net>
2016-07-17 23:56:10 -0800

    Add methods for bet call raise

--- Player.rb
+++ Player.rb
@@ -5,10 +5,21 @@ class Player
 		@chips = 0
 		@all_in = false
 		@type = 'computer'
+		@hand = []
 	end
 
 	def make_player_live()
 		@type = 'live'
+	end
+
+	def add_card(card)
+		@hand.push(card)
+	end
+
+	def show_hand()
+		@hand.each{ |card| print card.display() + " " }
+		print "\n"
+		end
 	end
 
 	def bet
